1eee25464165b217a06dd0c36f0059d015192bf4
SaiManideep00 <saimanideep00@gmail.com>
2024-06-06 15:38:09 -0500

    Many to one relationship

--- spring-data-jpa/src/test/java/com/example/spring_data_jpa/repository/CourseRepositoryTest.java
+++ spring-data-jpa/src/test/java/com/example/spring_data_jpa/repository/CourseRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.example.spring_data_jpa.repository;
 
+import com.example.spring_data_jpa.entity.Course;
+import com.example.spring_data_jpa.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,5 +19,20 @@ private CourseRepository courseRepository;
     public void displayCourseDetails()
     {
         System.out.println(courseRepository.findAll());
+    }
+
+    @Test
+    public void saveCourseWithTeacher()
+    {
+        Teacher teacher=Teacher.builder()
+                .firstName("Mukesh")
+                .lastName("Rishi")
+                .build();
+        Course course=Course.builder()
+                .courseTitle("Python")
+                .credits(4)
+                .teacher(teacher)
+                .build();
+        courseRepository.save(course);
     }
 }
