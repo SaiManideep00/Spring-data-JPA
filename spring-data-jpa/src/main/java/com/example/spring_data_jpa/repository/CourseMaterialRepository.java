a9eaa125cec2753696bd453acf61ffa3bb59d4e5
SaiManideep00 <95293895+SaiManideep00@users.noreply.github.com>
2025-07-01 13:23:59 -0400

    Delete spring-data-jpa directory

--- spring-data-jpa/src/main/java/com/example/spring_data_jpa/repository/CourseMaterialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring_data_jpa.repository;
-
-import com.example.spring_data_jpa.entity.CourseMaterial;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseMaterialRepository extends JpaRepository<CourseMaterial,Long> {
-}
